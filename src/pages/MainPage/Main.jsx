0d02814a712dbc4aa88732ce3d014d9991e45bb1
Alexei Kruk <alexei.kruk.dev@gmail.com>
2024-04-09 23:06:53 +0300

    add mainPage

--- src/pages/MainPage/Main.jsx
+++ src/pages/MainPage/Main.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './Main.css';
-
+import "./Main.css";
 
 export default function Main() {
     return (
-        <div className='main-container'>
-            <p className="title">Добро пожаловать на официльный сайт церкви</p>
-            <p className="title">Новый Завет</p>
-            <p className="title">От темноты до светла</p>
+        <div className="main-container">
+            <h3 className="title-welcome">
+                Добро пожаловать на официальный сайт церкви
+            </h3>
+            <h1 className="title-name">Новый Завет</h1>
+            <h3 className="title-tagline">От темноты до светла</h3>
         </div>
     );
 }
