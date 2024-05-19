5cdfddf963658051595b7bb3851073074fdbd410
Alexei Kruk <alexei.kruk.dev@gmail.com>
2024-05-19 09:03:08 +0300

    work with swiper

--- src/components/utilities/Swipers/SwiperBlog/SwiperBlog.jsx
+++ src/components/utilities/Swipers/SwiperBlog/SwiperBlog.jsx
@@ -11,23 +11,20 @@ import 'swiper/css';
 
 
 export const SwiperBlog = () => {
-    const videos = {
-        id: [1, 2, 3, 4, 5],
-        links: [
-            'https://www.youtube.com/embed/IYVFGSJTy5M?si=auxKyqCKnpQ4MkmK',
-            'https://www.youtube.com/embed/72UfFkMMmJk?si=z6RM0JA56WwZUfHB',
-            'https://www.youtube.com/embed/fQzaWDScfk0?si=k5cFA_uBoRl8-Z3q',
-            'https://www.youtube.com/embed/-Cl8WDn07_I?si=U9Aj4yXaXPNDze1B',
-            'https://www.youtube.com/embed/mXzIwisdUZY?si=Uy2uxFELX25sRfT2'
-        ]
-    };
+    const videos = [
+        'https://www.youtube.com/embed/IYVFGSJTy5M?si=auxKyqCKnpQ4MkmK',
+        'https://www.youtube.com/embed/72UfFkMMmJk?si=z6RM0JA56WwZUfHB',
+        'https://www.youtube.com/embed/fQzaWDScfk0?si=k5cFA_uBoRl8-Z3q',
+        'https://www.youtube.com/embed/-Cl8WDn07_I?si=U9Aj4yXaXPNDze1B',
+        'https://www.youtube.com/embed/mXzIwisdUZY?si=Uy2uxFELX25sRfT2'
+    ];
 
     return (
         <Swiper
             modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
             spaceBetween={400}
             slidesPerView={1}
-            autoplay={true}
+            autoplay={false}
             pagination={{ clickable: true }}
             onSwiper={(swiper) => console.log(swiper)}
             onSlideChange={() => console.log('slide change')}
@@ -35,14 +32,14 @@ export const SwiperBlog = () => {
             speed={800}
             a11y={{ enabled: true }}
         >
-            {videos['id'].map((index) => {
+            {videos.map((link, index) => {
                 return (
                     <SwiperSlide className="video-block" key={index}>
                         <iframe
                             className="video__item"
                             width={560}
                             height={315}
-                            src={videos['links'][index - 1]}
+                            src={link}
                             title="YouTube video player"
                             frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -51,7 +48,7 @@ export const SwiperBlog = () => {
                         ></iframe>
                     </SwiperSlide>
                 );
-            })}         
+            })}  
         </Swiper>
     ); 
 }
--- src/components/utilities/Swipers/SwiperSermons/SwiperSermons.jsx
+++ src/components/utilities/Swipers/SwiperSermons/SwiperSermons.jsx
@@ -1,59 +1,71 @@
 import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectCoverflow } from 'swiper/modules';
 
 import './../styles/Swipers.css';
 
 import 'swiper/swiper-bundle.css';
-import 'swiper/less/navigation';
-import 'swiper/less/pagination';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 import 'swiper/css';
 
 
 export const SwiperSermons = () => {
-    const sermons = {
-        id: [1, 2, 3, 4, 5],
-        links: [
+    const sermons = [
             'https://www.youtube.com/embed/F3BH6odTVrY?si=eczJ7p-N2lWtZ2ae',
             'https://www.youtube.com/embed/mMVtpA4DUkE?si=qq8VziUwSqDbhFuQ',
             'https://www.youtube.com/embed/Th4ye0hQBRw?si=lW6tUWqLSC9xHNkf',
             'https://www.youtube.com/embed/dhgl479jGFo?si=0y_a_Ez-9zpKBaND',
             'https://www.youtube.com/embed/zFFQrgjuf0M?si=DfFg1GqzK2U2_a43'
-        ]
-    };
+    ];
 
     return (
         <Swiper
-            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-            spaceBetween={400}
-            slidesPerView={1}
-            autoplay={true}
+            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectCoverflow]}
             pagination={{ clickable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-            loop
-            speed={800}
             a11y={{ enabled: true }}
+            navigation={{
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+                clickable: true,
+            }}
+            loop={true}
+            speed={800}
+            autoplay={false}
+            effect={'coverflow'}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={3}
+            coverflowEffect={{
+                rotate: 0,
+                stretch: 80,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            }}
         >
-            {sermons['id'].map((index) => {
+            {sermons.map((link, index) => {
                 return (
-                    <SwiperSlide className="video-block" key={index}>
-                        <iframe
-                            className="video__item"
-                            width={560}
-                            height={315}
-                            src={sermons['links'][index - 1]}
-                            title="YouTube video player"
-                            frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin"
-                            allowfullscreen
-                        ></iframe>
-                        
-                        {/* <div class="swiper-button-prev"></div>
-                        <div class="swiper-button-next"></div> */}
-                    </SwiperSlide>
+                    <div>
+                        <SwiperSlide className="video-block" key={index}>
+                            <iframe
+                                className="video__item"
+                                width={560}
+                                height={315}
+                                src={link}
+                                title="YouTube video player"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerpolicy="strict-origin-when-cross-origin"
+                                allowfullscreen
+                            ></iframe>
+                        </SwiperSlide>
+
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-button-next"></div>
+                    </div>
                 );
             })}         
         </Swiper>
